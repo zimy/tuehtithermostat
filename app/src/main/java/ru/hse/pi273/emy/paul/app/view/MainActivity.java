23eb2de3d89942720c5693d6da01c3a551f898e3
Dmitry 'Zimy' Yakovlev <zimy@yandex.ru>
2014-05-26 05:07:04 +0400

    There is a sample stub for "Add new task Activity"

---
+++ app/src/main/java/ru/hse/pi273/emy/paul/app/view/MainActivity.java
@@ -0,0 +1,48 @@
+package ru.hse.pi273.emy.paul.app.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import ru.hse.pi273.emy.paul.app.R;
+import ru.hse.pi273.emy.paul.app.view.task.CreateTaskActivity;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View click = findViewById(R.id.addtask);
+        click.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, CreateTaskActivity.class));
+    }
+}
---
+++ app/src/main/java/ru/hse/pi273/emy/paul/app/view/week/WeekListAdapter.java
@@ -0,0 +1,10 @@
+package ru.hse.pi273.emy.paul.app.view.week;
+
+/**
+ * Higher School of Economics
+ * Computer Science Faculty
+ * Created by Dmitry 'Zimy' Yakovlev
+ * on 26.05.14.
+ */
+public class WeekListAdapter {
+}
